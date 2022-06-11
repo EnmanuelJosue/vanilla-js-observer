ac6f7be2c3b5729df758f99ce8e3dc8c908f1e2a
Enmanuel <enmanueljosue2612@hotmail.com>
2022-06-11 11:10:08 -0400

    fetching data

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
 const API = 'https://api.escuelajs.co/api/v1/products';
 
+localStorage.setItem('offset', 5);
+let offset = localStorage.getItem('offset');
+
 const getData = api => {
   fetch(api)
     .then(response => response.json())
@@ -9,6 +12,16 @@ const getData = api => {
       let products = response;
       let output = products.map(product => {
         // template
+        return `
+          <article class="Card">
+            <img src=${product.images[0]} />
+            <h2>
+              ${product.title}
+              <small>$ ${product.price}</small>
+            </h2>
+          </article>
+        `;
+
       });
       let newItem = document.createElement('section');
       newItem.classList.add('Item');
@@ -19,11 +32,20 @@ const getData = api => {
 }
 
 const loadData = () => {
-  getData(API);
+  const limit = 5;
+  getData(`${API}?offset=${offset}&limit=${limit}`);
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
   // logic...
+  entries.forEach((entry) => {
+    if(entry.isIntersecting) {
+      loadData();
+      console.log('Intersectando');
+      offset = Number(localStorage.getItem('offset')) + 10;
+      localStorage.setItem('offset', offset);
+    }
+  })
 }, {
   rootMargin: '0px 0px 100% 0px',
 });
